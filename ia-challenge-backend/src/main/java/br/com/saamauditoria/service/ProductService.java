e7023be4d0d7eb2aa6c7a16930aed6e9a0ddd602
luisfelipefr <lfghias232@gmail.com>
2025-09-28 18:33:58 -0300

    refactor: mover operação de produto para ProductService

---
+++ ia-challenge-backend/src/main/java/br/com/saamauditoria/service/ProductService.java
@@ -0,0 +1,25 @@
+package br.com.saamauditoria.service;
+import br.com.saamauditoria.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import br.com.saamauditoria.model.Product;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() { return productRepository.findAll(); }
+
+    public Product save(Product product) {
+        product.setName(product.getName().trim());
+        product.setDescription(product.getDescription().trim());
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {productRepository.deleteById(id);}
+}
